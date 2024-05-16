7d5c4a0313f6eaba2deace6b5d6e4f518312f776
bellobambo <bellobambo21@gmail.com>
2024-05-16 05:28:56 +0100

    middleware

--- routes/posts.js
+++ routes/posts.js
@@ -18,7 +18,7 @@ let posts = [
 
 //GET ALL POST
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     const limit = parseInt(req.query.limit);
     if (!isNaN(limit) && limit > 0) {
         return res.status(200).json(posts.slice(0, limit));
@@ -29,52 +29,60 @@ router.get('/', (req, res) => {
 })
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
     if (!post) {
-        return res.status(404).json({ msg: `A post with id ${id} was not found` });
+        const error = new Error(`A post with id ${id} was not found`)
+        error.status = 404;
+         return next(error);
     }
     res.status(200).json(post)
 
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     const newPost = {
         id: posts.length + 1,
         title: req.body.title
     };
 
     if (!newPost.title) {
-        return res.status(400).json({ msg: 'Please include a title' })
+        const error = new Error(`Please Include a title`)
+        error.status = 400;
+         return next(error);
     }
     posts.push(newPost);
     res.status(201).json(newPost);
 });
 
 //update post
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
 
     const post = posts.find((post) => post.id === id);
 
     if (!post) {
-        return res.status(404).json({ msg: `A post with id ${id} was not found` });
+       const error = new Error(`A post with id ${id} was not found`)
+       error.status = 404;
+        return next(error);
     }
 
     post.title = req.body.title;
     res.status(200).json(posts);
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
 
     const post = posts.find((post) => post.id === id);
 
     if (!post) {
-        return res.status(404).json({ msg: `A post with id ${id} was not found` });
+        const error = new Error(`A post with id ${id} was not found`)
+        error.status = 404;
+         return next(error);
     }
 
     posts = posts.filter((post) => post.id !== id);
--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js'
 import logger from './middleware/logger.js'
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/Notfound.js';
 const port = process.env.PORT || 8000
 
 const app = express();
 
 //Body Parser
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
 
 //Logger mifddlware
@@ -17,7 +19,18 @@ app.use(logger);
 
 //Routes
 
-app.use('/api/posts' , posts)
+app.use('/api/posts', posts)
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+
 
 app.listen(port, () => {
     console.log('Server is running on port 8080');
